18c2a76903d33a582231ca64da1e136e15380576
orok <orok.ukpong@fixthub.com>
2018-12-11 19:29:28 -0500

    added async callbacks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class SearchBar extends Component {
-    state = {
-        term: '',
-    }
-    onFormSubmit = event => {
-        event.preventDefault();
-        console.log(this.state.term);
-    }
+  state = {
+    term: ""
+  };
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
 
-    render() {
-        return (
-            <div className=" ui segment">
-                <form className="ui form" onSubmit={this.onFormSubmit}>
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input type="text"
-                            onChange={(e) => this.setState({ term: e.target.value })}
-                            value={this.state.term} />
-                    </div>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className=" ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              onChange={e => this.setState({ term: e.target.value })}
+              value={this.state.term}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
